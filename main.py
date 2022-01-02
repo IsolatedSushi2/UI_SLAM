416f067d13ad8d54fe748200b6c24d3c8af84bdc
IsolatedSushi <s.vanherpt@students.uu.nl>
2022-01-02 14:17:04 +0100

    Refactoring

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from ui import Ui_MainWindow
-from src.slam import SLAM
+from src.ui.ui import Ui_MainWindow
+
+from src.ui.slamwindow import SLAMWindow
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 import sys
 
+
 if __name__ == "__main__":
+
+    # Create the window and setup the UI
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-    slam = SLAM(ui)
+
+    # Create and run the application
+    window = SLAMWindow(ui)
     sys.exit(app.exec_())
+
--- src/ui/slamwindow.py
+++ src/ui/slamwindow.py
@@ -1,16 +1,24 @@
 import os
+
 from src.data.extractors.dataExtractor import DataExtractor
+
 from src.ui.videoPageHandler import VideoPageHandler
+
 from src.ui.pointCloudPageHandler import PointCloudPageHandler
+
 from src.ui.cameraPageHandler import CameraPageHandler
+
 from src.camera import CameraParameters
 
+
 from src.constants import DEFAULT_DATASET_DIRECTORY, DEFAULT_CAMERA_PARAMETERS
+
 from src.constants import VIDEO_PAGE_INDEX, POINT_CLOUD_PAGE_INDEX, CAMERA_PAGE_INDEX
+
 cwd = os.getcwd()
 
 
-class SLAM:
+class SLAMWindow:
     def __init__(self, ui):
         self.ui = ui
         self.data = None
@@ -22,35 +30,59 @@ class SLAM:
     def connectLeftFrameButtons(self):
         # For changing the stackedwidget index
         self.ui.videoButton.clicked.connect(
+
             lambda: self.setMainStackedPage(VIDEO_PAGE_INDEX))
+
         self.ui.pointCloudButton.clicked.connect(
+
             lambda: self.setMainStackedPage(POINT_CLOUD_PAGE_INDEX))
+
         self.ui.camera3dButton.clicked.connect(
+
             lambda: self.setMainStackedPage(CAMERA_PAGE_INDEX))
 
+
         # Loading in the default dataset
+
         self.ui.uploadDefaultDatasetButton.clicked.connect(
+
             self.loadDefaultDirectory)
 
+
     # Load in a default dataset
+
     def loadDefaultDirectory(self):
+
         if(self.data):
             return
 
+
         self.loadDirectory(DEFAULT_DATASET_DIRECTORY)
 
+
     # Load in the data, and setup the widgets
+
     def loadDirectory(self, path):
+
 
         self.data = DataExtractor.loadDirectory(path, DEFAULT_CAMERA_PARAMETERS)
 
+
         print("Loaded in all data")
+
         self.ui.notificationLabel.setText("Loaded dataset: {}".format(path))
 
+
         self.videoPageHandler = VideoPageHandler(self.ui, self.data)
+
         #self.pointCloudPageHandler = PointCloudPageHandler(self.ui, self.data)
+
         self.cameraPageHandler = CameraPageHandler(self.ui, self.data)
 
+
     # Change the stackedWidget index
+
     def setMainStackedPage(self, index):
+
         self.ui.mainStackedWidget.setCurrentIndex(index)
+

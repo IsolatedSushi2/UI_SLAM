9e803de47b625d960837ccf6c88be5ab7dc323e6
IsolatedSushi <s.vanherpt@students.uu.nl>
2022-01-02 14:21:24 +0100

    refactored camera.py

--- src/camera.py
+++ src/camera.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+# Store the intrinsic parameters of the camera
 class CameraParameters:
     def __init__(self, width, height, focalx, focaly, centerx, centery, depthScalingFactor):
         self.focalx = focalx
@@ -18,23 +19,3 @@ class CameraParameters:
         return np.asmatrix([[self.focalx, 0, self.centerx],
                             [0, self.focaly, self.centery],
                             [0, 0, 1]])
-
-
-class Camera:
-    def __init__(self):
-        self.position = np.float64([0, 0, 0])
-        self.rotation = np.identity(3)
-        self.color = [0, 0, 0]
-
-        self.radius = 0.1
-        self.height = 0.15
-        self.resolution = 10
-        self.split = 3
-
-    def moveCamera(self, translation, rotation):
-        self.rotation = np.dot(rotation, self.rotation)
-        self.position = self.position + np.dot(1 * self.rotation, translation)
-        return self
-
-    def getRenderedCamera(self, last):
-        raise NotImplementedError()
